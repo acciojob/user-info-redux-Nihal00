993f1517424761fafb356b6b66aaec06c34f59d5
Nihal00 <nihallsuvarna@gmail.com>
2023-08-03 15:35:40 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <h1>User Information</h1>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" onChange={(e) => setName(e.target.value)}/>
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" onChange={(e) => setEmail(e.target.value)}/>
+      </div>
 
-export default App
+      <div>
+        <p>Current values in store:</p>
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
