41f66cd469129c5db4967d00e11699ec56817ac6
Nihal00 <nihallsuvarna@gmail.com>
2023-08-03 16:10:21 +0530

    three

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,28 @@
 import React, { useState } from "react";
 import "./../styles/App.css";
-import {useDispatch, useSeletor} from "react-redux"
 
 const App = () => {
-  
-  const dispatch = useDispatch();
-  const uname = useSeletor(state => state.name);
-  const uemail = useSeletor(state => state.email);
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  
   return (
     <div>
       {/* Do not remove the main div */}
       <h1>User Information</h1>
       <div>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" onChange={(e) => dispatch(e.target.value)}/>
+        <input type="text" id="name" onChange={(e) => setName(e.target.value)}/>
       </div>
       <div>
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" onChange={(e) => dispatch(e.target.value)}/>
+        <input type="text" id="email" onChange={(e) => setEmail(e.target.value)}/>
       </div>
 
       <div>
         <p>Current values in store:</p>
-        <p>Name - {uname}</p>
-        <p>Email - {uemail}</p>
+        <p>Name - {name}</p>
+        <p>Email - {email}</p>
       </div>
     </div>
   );
